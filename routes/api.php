7ed3bef37ddbff1f90488588fe5dfa247302d29d
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2023-12-11 17:02:09 -0300

    adress controller

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 // Listar os usuários
 Route::get('/user', [UserController::class, 'index']);
